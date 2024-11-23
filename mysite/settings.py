dbf098dcbcbcb55935a780d71dff6cd9db620cef
TheJackIsOnFire <alexsanderonfire@gmail.com>
2024-11-22 21:40:27 -0300

    run pagination

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,11 +128,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
